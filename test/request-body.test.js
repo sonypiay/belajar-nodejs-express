ff03d60649a9a35029526285192d1ec63fa2739e
sonypiay <pyscho30@gmail.com>
2024-10-08 01:52:34 +0700

    request body

---
+++ test/request-body.test.js
@@ -0,0 +1,42 @@
+import express from "express";
+import request from "supertest";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.post('/json', (req, res) => {
+    const name = req.body.name;
+    res.json({
+        message: `Hello ${name}`,
+    });
+});
+
+app.post('/form', (req, res) => {
+    const name = req.body.name;
+    res.json({
+        message: `Hello ${name}`,
+    });
+});
+
+test("Request Body Json", async () => {
+    const response = await request(app).post('/json')
+    .set('Content-Type', 'application/json')
+    .send({
+        name: 'World'
+    });
+
+    expect(response.body).toEqual({
+        message: "Hello World"
+    });
+});
+
+test("Request Body Form", async () => {
+    const response = await request(app).post('/form')
+    .set('Content-Type', 'application/x-www-form-urlencoded')
+    .send("name=World");
+
+    expect(response.body).toEqual({
+        message: "Hello World"
+    });
+});
