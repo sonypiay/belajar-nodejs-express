d6d687df117ccda6301ca4ebce82f3314a7cd0b9
sonypiay <pyscho30@gmail.com>
2024-10-07 23:34:28 +0700

    route function

---
+++ test/route-function.test.js
@@ -0,0 +1,32 @@
+import express from "express";
+import request from "supertest";
+
+const app = express();
+
+app.route('/products')
+.get((req, res) => {
+    res.send('Get Product');
+})
+.post((req, res) => {
+    res.send('Create Product');
+})
+.put((req, res) => {
+    res.send('Update Product');
+})
+.delete((req, res) => {
+    res.send('Delete Product');
+});
+
+test("Test Route Function", async () => {
+    let response = await request(app).get("/products");
+    expect(response.text).toBe("Get Product");
+
+    response = await request(app).post("/products");
+    expect(response.text).toBe("Create Product");
+
+    response = await request(app).put("/products");
+    expect(response.text).toBe("Update Product");
+
+    response = await request(app).delete("/products");
+    expect(response.text).toBe("Delete Product");
+})
